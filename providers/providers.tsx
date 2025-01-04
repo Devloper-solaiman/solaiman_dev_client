5ccabde436700d7231868a1662444e88143546ac
Devloper-solaiman <devlopersolaiman@gmail.com>
2025-01-04 22:52:00 +0600

    vercel deploy

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -1,29 +1,31 @@
 "use client";
 
-import type { ThemeProviderProps } from "next-themes";
-
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { Toaster } from "sonner";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
+
 // Create a client
 const queryClient = new QueryClient();
-
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
-      <Toaster/>
+      <Toaster />
       <QueryClientProvider client={queryClient}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
       </QueryClientProvider>
     </NextUIProvider>
   );
